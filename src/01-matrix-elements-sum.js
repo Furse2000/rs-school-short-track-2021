b0c593c5d0a141b6c2a38c365a1b85e111cf2c7c
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-04-13 01:41:06 +0300

    feat: add 1-st task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,12 +1,12 @@
 /**
- * Given matrix, a rectangular matrix of integers,
+ * Given mtrx, a rectangular mtrx of integers,
  * just add up all the values that don't appear below a "0".
  *
- * @param {Array<Array>} matrix
+ * @param {Array<Array>} mtrx
  * @return {Number}
  *
  * @example
- * matrix = [
+ * mtrx = [
  *  [0, 1, 1, 2],
  *  [0, 5, 0, 0],
  *  [2, 0, 3, 3]
@@ -14,8 +14,16 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(mtrx) {
+  let s = 0;
+  for (let i = 1; i < mtrx.length; i++) {
+    for (let j = 0; j < mtrx[0].length; j++) {
+      if (mtrx[i - 1][j] !== 0) {
+        s += mtrx[i][j];
+      }
+    }
+  }
+  return s + mtrx[0].reduce((a, b) => a + b, 0);
 }
 
 module.exports = getMatrixElementsSum;
