491adc36eae52d39913b7a96132ff03b54b13405
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-04-13 00:30:31 +0300

    feat: 11-th task completed

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let otvet = '';
+  let temp = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      temp++;
+    } else {
+      if (temp !== 1) {
+        otvet += `${temp}`;
+      }
+      otvet += str[i];
+      temp = 1;
+    }
+  }
+  return otvet;
 }
 
 module.exports = encodeLine;
