303715165432dc3c7521200abb7e0ee65a86f6c9
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-04-13 00:45:29 +0300

    feat: task add

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,14 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let ind = 0;
+  while (1) {
+    if (array[ind] === value) {
+      return ind;
+    }
+    ind++;
+  }
 }
 
 module.exports = findIndex;
